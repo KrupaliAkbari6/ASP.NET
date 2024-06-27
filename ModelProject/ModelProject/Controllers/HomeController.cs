a2dc42a48b065bb74e2719228dd1d4e5e131c0fd
KrupaliAkbari6 <krupali060802@gmail.com>
2024-06-27 17:10:45 +0530

    model

---
+++ ModelProject/ModelProject/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ModelProject.Models;
+
+namespace ModelProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Student s = new Student();
+            s.ID = 1;
+            s.Name = "Krupali";
+            s.City = "Rajkot";
+            return View(s);
+        }
+    }
+}
---
+++ ModelProject/ModelProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ModelProject.MvcApplication" Language="C#" %>
