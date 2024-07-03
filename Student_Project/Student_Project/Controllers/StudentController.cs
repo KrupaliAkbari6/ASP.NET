ae575f9e12c086f797fed59b1642f350d5e50889
KrupaliAkbari6 <krupali060802@gmail.com>
2024-07-03 18:34:46 +0530

    Add ViewBag

---
+++ Student_Project/Student_Project/Controllers/StudentController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Student_Project.Models;
+
+namespace Student_Project.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            student s = new student();
+            s.Id = 1;
+            s.Name = "Krupali";
+            s.City = "Rajkot";
+            s.Division = "A";
+            return View(s);
+        }
+        public ActionResult About()
+        {
+            ViewBag.id = new List<int>() { 1, 2, 3, 4, 5 };
+            ViewBag.name = new List<string>() { "Krupali", "Neha", "Vishakha", "Isha", "Kruti" };
+            ViewBag.city = new List<string>() { "Rajkot", "Gondal", "Rajkot", "Gondal", "Rajkot" };
+            ViewBag.division = new List<string>() { "A", "A", "A", "B", "B" };
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ Student_Project/Student_Project/Models/student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Student_Project.Models
+{
+    public class student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Division { get; set; }
+    }
+}
